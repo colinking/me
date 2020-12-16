ccf061b67cb6d7ee74640196b7f67f075bafb923
Colin <me@colinking.co>
2020-12-16 08:41:13 -0800

    remove webpack and add next analytics

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
+  analyticsId: 'BsRcTbEQozUQY6IDqLqBtbF5GQI',
+  // webpack: config => {
+  //   // Fixes npm packages that depend on `fs` module
+  //   config.node = {
+  //     fs: 'empty'
+  //   }
 
-    return config
-  }
+  //   return config
+  // }
 }
