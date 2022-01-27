1ba7a3747f4120f2d8838abb4047e45336eb49b7
Colin King <me@colinking.co>
2022-01-27 09:14:27 -0500

    Add blog redirect

---
+++ pages/l/authz-mental-model.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Redirect from '../../components/Redirect'
+
+const AuthzMentalModel = () => <Redirect title="The O(N^3) Authz Problem" url="https://colinking.substack.com/p/the-on3-authz-problem"/>
+
+export default AuthzMentalModel
