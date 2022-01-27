9dd3b49c564b9cab0052d1684aa57c9eab2a6245
Colin King <me@colinking.co>
2022-01-27 09:27:27 -0500

    force image refresh

--- pages/l/authz-mental-model.js
+++ pages/l/authz-mental-model.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Redirect from '../../components/Redirect'
 
-const AuthzMentalModel = () => <Redirect title="The O(N^3) Authz Problem" description="A mental model for building authz systems" url="https://colinking.substack.com/p/the-on3-authz-problem" image="/authz-problem.png"/>
+const AuthzMentalModel = () => <Redirect title="The O(N^3) Authz Problem" description="A mental model for building authz systems" url="https://colinking.substack.com/p/the-on3-authz-problem" image="/authz-problem.png#1"/>
 
 export default AuthzMentalModel
