84c3cf8c0f5543fa8db745ff4c066918a93ae669
Colin <me@colinking.co>
2026-04-10 17:17:32 -0700

    Enable Next.js Speed Insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
 import "../styles/twemoji-awesome.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
 }
