51543efc23fd5e51f326b40f13b76022acae7a3a
Colin <me@colinking.co>
2026-04-10 18:19:00 -0700

    Migrate to next/font (#27)
    This PR moves from directly importing fonts to using next/font instead.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Inconsolata } from "next/font/google";
 import type { AppProps } from "next/app";
 
 import "@/styles/globals.css";
 import "@/styles/twemoji-awesome.css";
 
+const inconsolata = Inconsolata({
+  subsets: ["latin"],
+  variable: "--font-inconsolata",
+  weight: ["400", "700"],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className={inconsolata.variable}>
       <Component {...pageProps} />
       <SpeedInsights />
-    </>
+    </div>
   );
 }
